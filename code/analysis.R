4f0a6bad7a3b7cf326d576a28bec18ab819e858a
dicook <visnut@gmail.com>
2023-02-24 13:50:56 +1100

    think we are done

---
+++ code/analysis.R
@@ -0,0 +1,64 @@
+library(lubridate)
+library(tidyverse)
+library(viridis)
+library(ggthemes)
+library(ggmap)
+library(plotly)
+
+# Read data
+numbats <- read_csv("data/numbats.csv")
+load("data/all_locations.rda")
+load("data/wa.rda")
+
+# Some basic analysis
+numbats %>% filter(year < 2000) %>%
+  select(year, decimalLongitude, decimalLatitude) %>%
+  arrange(year)
+
+# Locations across the country
+ggmap(all_loc) +
+  geom_point(data=numbats, 
+             aes(x=decimalLongitude, 
+                 y=decimalLatitude, 
+                 colour=year), 
+             alpha=0.4) +
+  scale_color_viridis(option="magma") +
+  theme_map() +
+  theme(legend.position="bottom")
+ggplotly()
+
+# Focus on the primary location of numbats
+ggmap(wa) +
+  geom_point(data=numbats, 
+             aes(x=decimalLongitude, 
+                 y=decimalLatitude,
+                 label=year), alpha=0.4) +
+  geom_density2d(data=numbats, 
+                 aes(x=decimalLongitude, 
+                     y=decimalLatitude) )+
+  theme_map()
+ggplotly()
+
+# Sighting times
+ggplot(numbats, aes(x=hour)) +
+  geom_density() +
+#  geom_histogram() +
+  scale_x_continuous(breaks=seq(0, 24, 2))
+
+numbats %>%
+  filter(!is.na(wday)) %>%
+  ggplot(aes(x=wday)) +
+  geom_bar()
+  
+numbats %>%
+  filter(!is.na(month)) %>%
+  ggplot(aes(x=month)) +
+  geom_bar()
+
+numbats %>%
+  filter(!is.na(year)) %>%
+  ggplot(aes(x=year)) +
+  geom_bar() +
+  xlim(c(2005, 2023))
+
+
--- code/data.R
+++ code/data.R
@@ -1,10 +1,7 @@
 library(galah) # API to ALA
 library(lubridate)
 library(tidyverse)
-library(viridis)
-library(ggthemes)
-library(ggmap)
-library(plotly)
+library(rnoaa)
 
 # Downloading data is free but you need to 
 # create an account https://www.ala.org.au then
@@ -22,79 +19,42 @@ numbats <- numbats %>%
          hour = hour(eventDate),
          day = ymd(as.Date(eventDate)))
 
-# Some basic analysis
-numbats %>% filter(year < 2000) %>%
-  select(year, decimalLongitude, decimalLatitude) %>%
-  arrange(year)
+narrogin <- meteo_pull_monitors(
+  monitors = "ASN00010614",
+  var = c("PRCP", "TMAX", "TMIN"),
+  date_min = "2005-01-01",
+  date_max = "2023-02-23")
 
-numbats %>%
-  ggplot(aes(x=decimalLongitude, y=decimalLatitude, colour=year)) +
-  geom_point() +
-  scale_color_viridis() +
-  theme_map()
+narrogin %>%
+  pivot_longer(cols = prcp:tmin, names_to = "var", values_to = "value") %>%
+  mutate(day = lubridate::yday(date), year = lubridate::year(date)) %>%
+  ggplot(aes(x = day, y= year, fill = is.na(value))) +
+  geom_tile() +
+  theme_minimal() +
+  theme(panel.grid = element_blank()) +
+  facet_wrap(vars(var), ncol = 1) +
+  scale_fill_brewer(palette = "Dark2", name = "missing") +
+  xlab("Day of the year")
 
-# Get a map
-all_loc <- get_map(c(115, -39, 151, -22))
-wa <- get_map(c(114, -36, 120, -30))
+narrogin_latlon <- tibble(lon = 117.1782, lat = -32.9310)
 
-ggmap(all_loc) +
-  geom_point(data=numbats, 
-             aes(x=decimalLongitude, 
-                 y=decimalLatitude, 
-                 colour=year), 
-             alpha=0.4) +
-  scale_color_viridis(option="magma") +
-  theme_map() +
-  theme(legend.position="bottom")
+within_rad <- function(x, y, lon, lat, km) {
+  deg <- km/111
+  inside <- ifelse(sqrt((lon-x)^2 + (lat-y)^2) < deg, "yes", "no")
+  return(inside)
+}
 
-ggplotly()
+# Only sites within 50km radius of Narrogin weather station
+# which is Dryandra Woodlands
+numbats <- numbats %>%
+  mutate(dryandra = within_rad(decimalLongitude, decimalLatitude, 
+                               narrogin_latlon$lon, narrogin_latlon$lat,
+                               50))
 
-ggmap(wa) +
-  geom_point(data=numbats, 
-             aes(x=decimalLongitude, 
-                 y=decimalLatitude,
-                 label=year), alpha=0.4) +
-  geom_density2d(data=numbats, 
-                 aes(x=decimalLongitude, 
-                     y=decimalLatitude) )+
-  theme_map()
-ggplotly()
-
-ggplot(numbats, aes(x=hour)) +
-  geom_density() +
-#  geom_histogram() +
-  scale_x_continuous(breaks=seq(0, 24, 2))
-
-numbats %>%
-  filter(!is.na(wday)) %>%
-  ggplot(aes(x=wday)) +
-  geom_bar()
-  
-numbats %>%
-  filter(!is.na(month)) %>%
-  ggplot(aes(x=month)) +
-  geom_bar()
-
-numbats %>%
-  filter(!is.na(year)) %>%
-  ggplot(aes(x=year)) +
-  geom_bar() +
-  xlim(c(2005, 2023))
-
+numbats <- numbats %>% 
+  left_join(narrogin, by=c("day"="date")) %>%
+  mutate(prcp = ifelse(dryandra == "no", NA, prcp),
+         tmax = ifelse(dryandra == "no", NA, tmax),
+         tmin = ifelse(dryandra == "no", NA, tmin)) %>%
+  select(-id)
 write_csv(numbats, file="numbats.csv")
-
-# Potential questions
-# What time of day do numbat sightings occur?
-# Was the distribution more widespread in the 1900s?
-# Do sightings happen more on week days than weekends?
-# Where do you find numbats in Australia?
-# Are numbats seen more on sunny and warm days than cloudy, wet, cold days? (NEED WEATHER DATA)
-# Are they more frequent in the summer or winter?
-# Who/what are/is the most diligent collector?
-
-# Distribute
-# Code to create or update
-# Processed variables
-# Australian/WA map
-# weather variable indicating cold and dark
-
